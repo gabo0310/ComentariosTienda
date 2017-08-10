cd177b9be5b444951a511789d63ace8334696352
Gabriel Jaime Noreña Ramirez <gabrielnorena@MacBook-Pro-de-Gabriel.local>
2017-08-10 03:01:35 -0500

    Creacion de referencia para obtener el valor

--- Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.gabrielnorena.firebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.gabrielnorena.firebase.Objetos.FirebaseReferences;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class  MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,17 @@ public class  MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase base_datos = FirebaseDatabase.getInstance();
-        DatabaseReference referencia = base_datos.getReference();
+        DatabaseReference referencia = base_datos.getReference(FirebaseReferences.TUTORIAL_REFERENCE);
+        referencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        })
     }
 }
--- Firebase/app/src/main/java/com/example/gabrielnorena/firebase/Objetos/FirebaseReferences.java
+++ Firebase/app/src/main/java/com/example/gabrielnorena/firebase/Objetos/FirebaseReferences.java
@@ -4,6 +4,6 @@ package com.example.gabrielnorena.firebase.Objetos;
  * Created by gabrielnorena on 10/08/17.
  */
 public class FirebaseReferences {
-    //En este caso se debe declarar el nombre de la variable en mayuscual porque es un TAG
+    //En este caso se debe declarar la variable asi porque es un TAG
     final public static String TUTORIAL_REFERENCE = "tutorial";
 }
