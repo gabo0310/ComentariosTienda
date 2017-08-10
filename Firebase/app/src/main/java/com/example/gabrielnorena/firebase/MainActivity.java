97a70142399c3061f35984eb32fcb6b82f3eef26
Gabriel Jaime Noreña Ramirez <gabrielnorena@MacBook-Pro-de-Gabriel.local>
2017-08-10 02:54:38 -0500

    Creando instancia de la base de datos de firebase

--- Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.gabrielnorena.firebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase base_datos = FirebaseDatabase.getInstance();
+        DatabaseReference referencia = base_datos.getReference();
     }
 }
