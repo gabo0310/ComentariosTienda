9423920fdf77344575b26436e91d1cc0b6b8dd94
Gabriel Jaime Noreña Ramirez <gabrielnorena@MacBook-Pro-de-Gabriel.local>
2017-08-10 03:46:43 -0500

    Creacion de boton en MainActivity y cracion de evento del boton

--- Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gabrielnorena.firebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gabrielnorena.firebase.Objetos.Coche;
 import com.example.gabrielnorena.firebase.Objetos.FirebaseReferences;
@@ -14,13 +16,24 @@ import com.google.firebase.database.ValueEventListener;
 
 public class  MainActivity extends AppCompatActivity {
 
+    Button boton_coche;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        boton_coche = (Button) findViewById(R.id.boton_coche);
+
         FirebaseDatabase base_datos = FirebaseDatabase.getInstance();
         DatabaseReference tutorialRef = base_datos.getReference(FirebaseReferences.TUTORIAL_REFERENCE);
+
+        boton_coche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
         tutorialRef.child(FirebaseReferences.COCHE_REFERENCE).addValueEventListener(new ValueEventListener() {
             @Override
