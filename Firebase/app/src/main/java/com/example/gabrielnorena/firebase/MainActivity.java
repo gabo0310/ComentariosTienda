f6ba77b3b640b66e9176ecdd5cea27a27a9cc909
Gabriel Jaime Noreña Ramirez <gabrielnorena@MacBook-Pro-de-Gabriel.local>
2017-08-09 22:07:52 -0500

    Agregando el SDK para las bibliotecas de Firebase

--- Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.gabrielnorena.firebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
