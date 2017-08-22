13562a5974cce7a626b3cd0321699c7f48aa92d9
Gabriel Jaime Noreña Ramirez <gabrielnorena@MacBook-Pro-de-Gabriel.local>
2017-08-21 19:49:50 -0500

    Creacion de referencias de botones y edittext, y cracion de metodos para registro y inicio de sesion en firebase

--- Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/example/gabrielnorena/firebase/MainActivity.java
@@ -5,25 +5,55 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.gabrielnorena.firebase.Objetos.Coche;
 import com.example.gabrielnorena.firebase.Objetos.FirebaseReferences;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class  MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    Button btnRegistrarse, btnIngresar;
+    EditText txtEmail, txtClave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnRegistrarse = (Button) findViewById(R.id.boton_registrarse);
+        btnIngresar = (Button) findViewById(R.id.boton_ingresar);
+        txtEmail = (EditText) findViewById(R.id.login_email);
+        txtClave = (EditText) findViewById(R.id.login_password);
+
+        btnRegistrarse.setOnClickListener(this);
+        btnRegistrarse.setOnClickListener(this);
 
 
+
+    }
+
+
+    private void registrar(String email, String clave){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,clave);
+    }
+
+    private void iniciarSesion(String email, String clave){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,clave);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.boton_ingresar:
+                break;
+            case R.id.boton_registrarse:
+                break;
+        }
     }
 }
